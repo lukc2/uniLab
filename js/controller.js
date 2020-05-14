772376fa5a18885da9ac27c6781276fea6c832c3
lukc2 <lukc2@wp.pl>
2020-05-14 22:29:10 +0200

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,24 @@
+var portfolioApp = angular.module('portfolioApp',[]);
+
+portfolioApp.controller('GalleryListCtrl',GalleryListCtrl);
+portfolioApp.controller('HeaderCtrl',HeaderCtrl);
+function GalleryListCtrl($http){
+
+    var vm = this;
+
+    $http.get('./json/galleries.json').then(function(response){
+        vm.galleries=response.data;
+    });
+    $http.get('./json/sortTypes.json').then(function(response){
+        vm.sortList = response.data;
+        vm.orderProp=vm.sortList[1];
+    });
+    }
+function HeaderCtrl($http){
+    var hd = this;
+    $http.get('./json/headers.json').then(function(response){
+        hd.list = response.data;
+    });
+
+}
+
